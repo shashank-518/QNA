33fb40d3de34041563f877b4dd7bdf0cb20ff4c3
Shashank S <shashank5418shashu@gmail.com>
2025-06-23 21:36:45 +0530

    changed

--- my-app/src/api/vote/route.ts
+++ my-app/src/api/vote/route.ts
@@ -1,7 +1,8 @@
-import { db, voteCollection } from "@/models/name";
-import { databases } from "@/models/Server/config";
+import { answerCollection, db, questionCollection, voteCollection } from "@/models/name";
+import { databases, users } from "@/models/Server/config";
+import { UserPrefs } from "@/Store/auth";
 import { NextRequest, NextResponse } from "next/server";
-import { Query } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 
 export default async function POST(request:NextRequest){
 
@@ -18,11 +19,61 @@ export default async function POST(request:NextRequest){
 
         if(response.documents.length > 0){
 
+            await databases.deleteDocument(db , voteCollection , response.documents[0].$id )
+
+            const questionoranswer = await databases.getDocument(
+                db ,
+                type ==="question" ? questionCollection : answerCollection,
+                typeId
+            )
+
+            const authPref = await users.getPrefs<UserPrefs>(questionoranswer.authorId)
+
+            await users.updatePrefs<UserPrefs>(questionoranswer.authorId , {
+                reputation : voteStatus === "upvoted" ? Number(authPref.reputation) - 1 :
+                Number(authPref.reputation) + 1 
+            })
+
+
         }
 
         if(response.documents[0]?.voteStatus !== voteStatus){
 
+            const docs = await databases.createDocument(
+                db , voteCollection , ID.unique() ,{
+                    votedById,
+                    type,
+                    typeId,
+                    voteStatus
+                }
+            )
+
+            const questionoranswer = await databases.getDocument(
+                db ,
+                type ==="question" ? questionCollection : answerCollection,
+                typeId
+            )
+
+            const authPref = await users.getPrefs<UserPrefs>(questionoranswer.authorId)
+
+             if (response.documents[0]) {
+                await users.updatePrefs<UserPrefs>(questionoranswer.authorId, {
+                    reputation:
+                        // that means prev vote was "upvoted" and new value is "downvoted" so we have to decrease the reputation
+                        response.documents[0].voteStatus === "upvoted"
+                            ? Number(authPref.reputation) - 1
+                            : Number(authPref.reputation) + 1,
+                })
+            } else {
+                await users.updatePrefs<UserPrefs>(questionoranswer.authorId, {
+                    reputation:
+                        // that means prev vote was "upvoted" and new value is "downvoted" so we have to decrease the reputation
+                        voteStatus === "upvoted"
+                            ? Number(authPref.reputation) + 1
+                            : Number(authPref.reputation) - 1,
+                })
         }
+    }
 
         const [upvotes,downvotes] = await Promise.all([
             await databases.listDocuments(
